3a33345193aaf9fe44d5615fe88c99fb6e83248c
DESKTOP-6RA1I0Q\nhutt <nhuttantranle@gmail.com>
2024-03-07 11:47:55 +0700

    all products

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -15,43 +15,11 @@ document.addEventListener("click", (e) => {
 });
 
 
-const listImages = document.querySelector('.list-images')
-const imgs = document.querySelectorAll('.img-div')
-const btnLeft = document.getElementById('btn-left')
-const btnRight = document.getElementById('btn-right')
-let current = 0 
+// Xử lí thanh trượt giá
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
-const handleChangeSLide = () => {
-  if (current == imgs.length - 1){
-    current == 0
-    // let width = imgs[0].offsetWidth
-    let width = 1000
-    listImages.style.transform = `translateX(0px)`
-  }else{
-    current++
-    // let width = imgs[0].offsetWidth
-    let width = 500
-    listImages.style.transform = `translateX(${width * -1 * current}px)`
-  }
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
-btnRight.addEventListener('click', () => {
-  clearInterval(handleChangeSLide)
-  handleChangeSLide()
-  handleEventChangeSlide = setInterval(() => {handleChangeSLide}, 4000)
-})
-btnLeft.addEventListener('click', () => {
-  clearInterval(handleChangeSLide)
-  if (current == 0){
-    current == length - 1
-    // let width = imgs[0].offsetWidth
-    let width = 500
-    listImages.style.transform = `translateX(${width * -1 * current}px)`
-  }else{
-    current--
-    // let width = imgs[0].offsetWidth
-    let width = 500
-    listImages.style.transform = `translateX(${width * -1 * current}px)`
-  }
-  handleEventChangeSlide = setInterval(() => {handleChangeSLide}, 4000)
-})
-let handleEventChangeSlide = setInterval(() => {handleChangeSLide}, 4000)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,71 +2,60 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      'background-200': '#F6F5ED',
-      'background-400': '#F1EFE6',
-      'dark': '#000000',
-      'white': '#FFFFFF',
-      'slate':{
-        '400': 'rgb(148 163 184)',
-        '600': 'rgb(71 85 105)',
-        '800': 'rgb(30 41 59)'
+    extend:{
+      width: {
+        'calc': 'calc',
       },
-      'gold':{
-        '200': '#C19A5B',
-        '400': '#B48B48'
-      } 
-    },
-    keyframes:{
-      slideLeft:{
-        '0%': {transform: 'translateX(100%)'},
-        '100%': {transform: 'translateX(0%)'}
-      },
-      fadeIn:{
-        from: {
-          opacity: 0
+      colors: {
+        'background-200': '#F6F5ED',
+        'background-400': '#F1EFE6',
+        'dark': '#000000',
+        'gold':{
+          '200': '#C19A5B',
+          '400': '#B48B48'
         },
-        to:{
-          opacity: 1
-        }
       },
-      slideRight:{
-        '0%': {transform: 'translateX(0%)'},
-        '100%': {transform: 'translateX(100%)'}
-      },
-      fadeOut:{
-        from: {
-          opacity: 1
+      keyframes:{
+        slideLeft:{
+          '0%': {transform: 'translateX(100%)'},
+          '100%': {transform: 'translateX(0%)'}
         },
-        to:{
-          opacity: 0
-        }
+        fadeIn:{
+          from: {
+            opacity: 0
+          },
+          to:{
+            opacity: 1
+          }
+        },
+        slideRight:{
+          '0%': {transform: 'translateX(0%)'},
+          '100%': {transform: 'translateX(100%)'}
+        },
+        fadeOut:{
+          from: {
+            opacity: 1
+          },
+          to:{
+            opacity: 0
+          }
+        },
       },
-    },
-    animation:{
-      slideLeft: 'slideLeft 0.4s ease-in-out',
-      fadeIn: 'fadeIn 0.5s ease-in-out',
-      slideRight: 'slideRight 0.4s ease-in-out',
-      fadeOut: 'fadeOut 0.5s ease-in-out'
-    },
-    screens:{
-      'xs': '480px',
-
-      'sm': '640px',
-
-      'md': '768px',
-
-      'lg': '1024px',
-
-      'xl': '1280px',
-
-      '2xl': '1536px',
-    },
-    translate: {
-      '40%': '40%',
-    },
-    fontWeight:{
-      'dropdown-menu': '400'
+      animation:{
+        slideLeft: 'slideLeft 0.4s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        slideRight: 'slideRight 0.4s ease-in-out',
+        fadeOut: 'fadeOut 0.5s ease-in-out'
+      },
+      screens:{
+        'xs': '480px',
+      },
+      translate: {
+        '40%': '40%',
+      },
+      fontWeight:{
+        'dropdown-menu': '400'
+      },
     }
   },
   plugins: [],
